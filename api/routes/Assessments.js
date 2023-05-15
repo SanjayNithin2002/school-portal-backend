4c976c30c8c52b1b8d3e3a910e06ef616b9f2773
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-05-15 20:38:04 +0530

    environment variables added

--- api/routes/Assessments.js
+++ api/routes/Assessments.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var express = require('express');
 var multer = require('multer');
 var admin = require("firebase-admin");
-var serviceAccount = require("../../serviceAccountKey.json");
 var Students = require('../models/Students');
 var Assessments = require('../models/Assessments');
 var checkAuth = require('../middleware/checkAuth');
@@ -36,7 +35,19 @@ const upload = multer({
     },
     fileFilter: fileFilter
 });
-
+const serviceAccount = {
+    type: process.env.type,
+    project_id : process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key : process.env.private_key,
+    client_email: process.env.client_email,
+    client_id : process.env.client_id,
+    auth_uri : process.env.auth_uri,
+    token_uri : process.env.token_uri,
+    auth_provider_x509_cert_url : process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url : process.env.client_x509_cert_url,
+    universe_domain : process.env.universe_domain
+}
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: process.env.BUCKET_URL
--- api/routes/Bonafides.js
+++ api/routes/Bonafides.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var express = require('express');
 var multer = require('multer');
 var admin = require("firebase-admin");
-var serviceAccount = require("../../serviceAccountKey.json");
 var Bonafides = require('../models/Bonafides');
 var checkAuth = require('../middleware/checkAuth');
 var router = express.Router();
@@ -35,6 +34,20 @@ const upload = multer({
     },
     fileFilter: fileFilter
 });
+
+const serviceAccount = {
+    type: process.env.type,
+    project_id : process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key : process.env.private_key,
+    client_email: process.env.client_email,
+    client_id : process.env.client_id,
+    auth_uri : process.env.auth_uri,
+    token_uri : process.env.token_uri,
+    auth_provider_x509_cert_url : process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url : process.env.client_x509_cert_url,
+    universe_domain : process.env.universe_domain
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
