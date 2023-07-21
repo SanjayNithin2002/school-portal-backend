f2f405f00b0ca7da9115be5325c258e68d9ffe14
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-07-21 10:14:25 +0530

    teacher uniqueness removed

--- api/models/Teachers.js
+++ api/models/Teachers.js
@@ -6,11 +6,7 @@ var teacherSchema = mongoose.Schema({
         type : String, 
         required : true
     },
-    email : {
-        type : String,
-        required : true,
-        unique : true
-    },
+    email : String,
     userID : {
         type : String,
         required : true,
