b633e14cb9348efc8dd67a213ea102f724863414
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-08-29 21:11:21 +0530

    brushing up

--- api/routes/Admins.js
+++ api/routes/Admins.js
@@ -49,11 +49,11 @@ var serviceAccount = {
     private_key: process.env.private_key.replace(/\\n/g, '\n'),
     client_email: process.env.client_email,
     client_id: process.env.client_id,
-    auth_uri: process.env.auth_uri,
-    token_uri: process.env.token_uri,
-    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     client_x509_cert_url: process.env.client_x509_cert_url,
-    universe_domain: process.env.universe_domain
+    universe_domain: "googleapis.com"
 }
 
 admin.initializeApp({
